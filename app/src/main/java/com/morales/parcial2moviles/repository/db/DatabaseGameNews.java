b268f959b216a7376f86afcbbcf498ee61214560
Karla <00022516@uca.edu.sv>
2018-06-10 15:33:52 -0600

    Comments

--- app/src/main/java/com/morales/parcial2moviles/repository/db/DatabaseGameNews.java
+++ app/src/main/java/com/morales/parcial2moviles/repository/db/DatabaseGameNews.java
@@ -12,22 +12,32 @@ import com.morales.parcial2moviles.repository.modelo.New;
  */
 
 @Database(entities = {New.class}, version = 1)
+//Creamos metodo abstracto que extiende de room
 public abstract class DatabaseGameNews extends RoomDatabase {
 
+    //Inicializamos el DAO
     public abstract NewDAO newDAO();
 
+
+    //Instanciamos la base de datos
     private static DatabaseGameNews INSTANCE;
 
     public static DatabaseGameNews getDatabase(final Context context) {
+        //Verificamos si la instancia es nula
         if (INSTANCE == null) {
+
+            //Es una instancia la cual pregunta si no hay otro trhead actuando
+            //Se sincroniza la clase actual de base de datos
             synchronized (DatabaseGameNews.class) {
                 if (INSTANCE == null) {
+                    //Si la instancia es nula entonces obtenemos el contexto de la aplicacion y lo construimos
                     INSTANCE = Room.databaseBuilder(context.getApplicationContext(),
                             DatabaseGameNews.class, "new_database")
                             .build();
                 }
             }
         }
+        //Retornamos la instancia
         return INSTANCE;
     }
 
